d4a160aa8d438a536f225b6c97c618c6d8f46adc
Lori Marshall <lorettamarshall123@yahoo.com>
2014-06-30 13:16:17 -0500

    added master file to render HTML format

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -1,9 +1,20 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <title>Laravel Blog</title>
-</head>
-<body>
-    @yield('content')
-</body>
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<meta charset=”utf-8”> 
+    	<title>@yield('title')</title>
+		    	
+      @yield('topscript')
+    </head>
+
+  @if(!isset($route))
+    <body>
+  @elseif($route == 'portfolio')
+	  <body data-spy="scroll" data-offset="0" data-target="#theMenu">
+  @endif
+  
+    	@yield('content')
+      @yield('bottomscript')
+  </body>
 </html>
